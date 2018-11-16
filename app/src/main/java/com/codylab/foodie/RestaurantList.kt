2f58f126e4269fb4c475a99fe5ca1f225349e1e9
Cody Liu <liu@codylab.com>
2018-11-17 09:58:30 +1100

    Initial commit

---
+++ app/src/main/java/com/codylab/foodie/RestaurantList.kt
@@ -0,0 +1,12 @@
+package com.codylab.foodie
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RestaurantList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_list)
+    }
+}
